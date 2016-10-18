b2a71d97b2fdea25878d9ef7d1f176076591e317
John Colagioia <john@colagioia.net>
2016-10-18 07:01:08 -0400

    Add text to images

--- video.sh
+++ video.sh
@@ -7,36 +7,48 @@
 image=$1
 time=$2
 lineread=$3
+font=$4
 
 fname=$(basename "$image")
 size=$(identify "$image" | cut -f3 -d' ')
 wd=$(echo $size | cut -f1 -d'x')
 ht=$(echo $size | cut -f2 -d'x')
+geo=+1+1
+linelen=100
 
 # if portrait, dir should be east or west; if landscape, north or south
-rr=$(rand -M 2)
+# position of the line needs to be opposite it
+rr=$(rand --max 2)
 if [ $ht -ge $wd ]
 then
+  linelen=50
   if [ $rr -eq 0 ]
   then
     dir=east
+    geo=+100+200
   else
     dir=west
+    geo=+1180+200
   fi
 else
+  linelen=340
   if [ $rr -eq 0 ]
   then
     dir=north
+    geo=+100+100
   else
     dir=south
+    geo=+100+750
   fi
 fi
+
+lineread=$(echo "$lineread" | fold --space --width=$linelen)
 
 # need 1920 x 1080
 convert "$image" -resize 1920x1080 -gravity $dir -background lightgray -extent 1920x1080 "resized_$fname"
 
-# Add spoken text
-# http://www.imagemagick.org/Usage/text/
+# Add text
+convert "resized_$fname" -fill black -font "$font" -pointsize 48 -annotate $geo "$lineread" "text_$fname"
 
-ffmpeg -framerate 1/$time -i "resized_$fname" -c:v libx264 -r 30 test.mp4
+ffmpeg -framerate 1/$time -i "text_$fname" -c:v libx264 -r 30 test.mp4
 
