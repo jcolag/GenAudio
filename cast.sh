a7a82edc1988faceb7483aecb57a5cdf2c74e0f3
John Colagioia <john@colagioia.net>
2016-11-17 05:15:02 -0500

    Use cast scan to pick up locations

--- cast.sh
+++ cast.sh
@@ -3,4 +3,5 @@
 # do associate text-to-speech voices
 mv chars.txt chars.prev.txt
 grep "^[A-Z][A-Z]" "$1" | grep -v "[:.^]" | cut -f1 -d' ' | sort -u | sed 's/$/,/g' > chars.txt
-
+mv bgcolors.csv bgcolors.prev.csv
+grep "^[A-Z]*\." /home/john/Documents/jay/Trek/scripts/1x01\ Armageddon.fountain | sort | uniq | sed 's/$/,/g' > bgcolors.csv
