6e9a968583b6bda6af362197cbfa5422f56b00da
John Colagioia <john@colagioia.net>
2016-11-19 05:58:22 -0500

    Read background colors for video

--- allvideo.sh
+++ allvideo.sh
@@ -3,6 +3,7 @@ play=$1
 font=$2
 lines=whospeak.csv
 chars=chars.txt
+colors=colors.csv
 start=$(date)
 
 while read -r line
@@ -20,10 +21,15 @@ do
     tspeaker=$(echo $speaker | cut -f1 -d' ')
     image=$(grep "^$tspeaker," "$chars" | cut -f3 -d',')
   fi
+  # ...And the location
+  loc=$(head --lines=$num "$play" | grep -n "^[A-Z]*\. " | tail --lines=1 | cut -f2 -d':')
+  # ...And the location's background and font color
+  bg=$(grep "^$loc," "$colors" | cut -f2 -d',')
+  fg=$(grep "^$loc," "$colors" | cut -f3 -d',')
   # Call the video conversion script with the right parameters
   # As far as I can tell, the JavaScript code runs asynchronously, making
   # it a disaster in a loop "locally."  This is, therefore, a stopgap.
-  node video.js --font="$2" --image="$image" --line=$num --play="$play"
+  node video.js --background="$bg" --color="fg" --font="$2" --image="$image" --line=$num --play="$play"
   echo $num
 done < "$lines"
 echo $start
