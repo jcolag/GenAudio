1e95f8bc8f3e0a623317e1324c4a5d9da85ea5e4
John Colagioia <john@colagioia.net>
2016-11-24 15:23:17 -0500

    Add obvious arguments and start on main code

--- credits.js
+++ credits.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const cp = require('child_process');
+const im = require('imagemagick');
 const readline = require('readline');
 const commandLineArgs = require('command-line-args');
 
@@ -7,16 +8,29 @@ const outDir = './clips';
 
 const optionDefinitions = [
   { name: 'delimiter', alias: 'd', type: String },
-  { name: 'infile', alias: 'i', type: String },
+  { name: 'font', alias: 'f', type: String },
+  { name: 'music', alias: 'm', type: String },
+  { name: 'text', alias: 't', type: String },
 ];
 const options = commandLineArgs(optionDefinitions);
 
-var filename = options.infile;
+var text = options.text;
+var font = options.font;
+var music = options.music;
 var delimiter = options.hasOwnProperty('delimiter') ? options.delimiter : '---';
 
-if (!options.hasOwnProperty('infile')) {
+if (!options.hasOwnProperty('font')
+ || !options.hasOwnProperty('music')
+ || !options.hasOwnProperty('text')) {
   // Can't do anything useful without input
   return;
 }
 
+var lineReader = readline.createInterface({
+  terminal: false,
+  input: fs.createReadStream(filename)
+});
+var screenful = '';
+lineReader.on('line', function (line) {
+}
 
