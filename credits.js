d4623caaefcae5edf1d32bf4370f39e6b25c80fe
John Colagioia <john@colagioia.net>
2016-12-03 08:00:30 -0500

    Begin parsing credit text

--- credits.js
+++ credits.js
@@ -9,15 +9,19 @@ const outDir = './clips';
 const optionDefinitions = [
   { name: 'delimiter', alias: 'd', type: String },
   { name: 'font', alias: 'f', type: String },
+  { name: 'justify', alias: 'j', type: String },
   { name: 'music', alias: 'm', type: String },
+  { name: 'size', alias: 's', type: Number },
   { name: 'text', alias: 't', type: String },
 ];
 const options = commandLineArgs(optionDefinitions);
 
-var text = options.text;
+var delimiter = options.hasOwnProperty('delimiter') ? options.delimiter : '===';
 var font = options.font;
+var justify = options.hasOwnProperty('justify') ? options.justify : 'c';
 var music = options.music;
-var delimiter = options.hasOwnProperty('delimiter') ? options.delimiter : '---';
+var fontsize = options.hasOwnProperty('size') ? options.size : '48';
+var text = options.text;
 
 if (!options.hasOwnProperty('font')
  || !options.hasOwnProperty('music')
@@ -26,11 +30,26 @@ if (!options.hasOwnProperty('font')
   return;
 }
 
+var proc = cp.spawnSync('/usr/bin/soxi', [ '-D', music ]);
+var time = parseFloat(proc.stdout.toString().trim(), 10);
+
 var lineReader = readline.createInterface({
   terminal: false,
-  input: fs.createReadStream(filename)
+  input: fs.createReadStream(text)
 });
-var screenful = '';
+var screens = [];
+var screenful = [];
 lineReader.on('line', function (line) {
-}
+  if (line == delimiter) {
+    screens.push(screenful);
+    screenful = [];
+  } else {
+    screenful.push(line);
+  }
+});
+lineReader.on('close', function() {
+  var pages = screens.length;
+  var timePerPage = time / pages;
+  console.log('Runs for ' + time + 's, or ' + timePerPage + 's per page.');
+});
 
